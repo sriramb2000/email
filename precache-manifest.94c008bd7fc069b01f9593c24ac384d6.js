a684a9befbb299c5559528d9269f009badd7fd06
Sriram Balachandran <sriramb2000@gmail.com>
2020-06-04 20:27:36 -0700

    Updates

---
+++ precache-manifest.94c008bd7fc069b01f9593c24ac384d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e9aebdac7b1cffcc87a27f966f9066",
+    "url": "/email/index.html"
+  },
+  {
+    "revision": "db13bdc55f829fe2e380",
+    "url": "/email/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4292b61c39cd38834ff6",
+    "url": "/email/static/js/2.2ce8bcef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/email/static/js/2.2ce8bcef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db13bdc55f829fe2e380",
+    "url": "/email/static/js/main.a404a60b.chunk.js"
+  },
+  {
+    "revision": "0be499aa72bf41104465",
+    "url": "/email/static/js/runtime-main.f8a3cbe3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/email/static/media/logo.5d5d9eef.svg"
+  }
+]);
